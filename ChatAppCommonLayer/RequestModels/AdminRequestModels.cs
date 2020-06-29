331a38700acb5b8e118c0fcdc85653a3d1736a9d
vinayakushakola <vinayak.ushakola@gmail.com>
2020-06-29 10:47:32 +0530

    Added: Admin Request & Response Models

---
+++ ChatAppCommonLayer/RequestModels/AdminRequestModels.cs
@@ -0,0 +1,40 @@
+﻿//
+// Author   : Vinayak Ushakola
+// Date     : 29 June 2020
+// Purpose  : It Contains Model that Admin may Requests
+//
+
+using System.ComponentModel.DataAnnotations;
+
+namespace ChatAppCommonLayer.RequestModels
+{
+    public class AdminRegistrationRequest
+    {
+        [Required]
+        [MinLength(3, ErrorMessage = "Your FirstName Length Should be more than 3")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "Your LastName Length Should be more than 3")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Please Enter a Proper Email-ID")]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(5, ErrorMessage = "Your Password Should be Minimum Length of 5")]
+        public string Password { get; set; }
+    }
+
+    public class AdminLoginRequest
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Please Enter a Proper Email-ID")]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(5, ErrorMessage = "Your Password Should be Minimum Length of 5")]
+        public string Password { get; set; }
+    }
+}
---
+++ ChatAppCommonLayer/ResponseModels/AdminResponseModels.cs
@@ -0,0 +1,29 @@
+﻿//
+// Author   : Vinayak Ushakola
+// Date     : 29 June 2020
+// Purpose  : It Contains Admin Response Models
+//
+
+using System;
+
+namespace ChatAppCommonLayer.ResponseModels
+{
+    public class AdminRegistrationResponse
+    {
+        public int AdminID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string UserRole { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+    }
+}
